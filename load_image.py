a762b3dc7b3994e5ea2910a18da8052f3a20d0c6
Karanveer Singh <karsingh306@gmail.com>
2019-11-17 04:53:22 +0000

    read to train

--- load_image.py
+++ load_image.py
@@ -13,6 +13,7 @@ class DataGen(keras.utils.Sequence):
         self.batch_size = batch_size
         self.is_validation = is_validation
         file_list = [] 
+        self.path = path
         for root, dirs, files in os.walk(path):
             for folder in dirs:
                 image_path = path + '/' + folder + "/imaging.nii.gz"
@@ -20,41 +21,49 @@ class DataGen(keras.utils.Sequence):
                 case_id = folder[-3:]
                 self.df = self.df.append({'case_id':int(case_id), 'image':image_path, 'mask':mask_path}, ignore_index=True)
         self.df = self.df.sort_values(by=['case_id'])
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-            print(self.df)
-
-
+        #with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+            #print(self.df)
+    
     def __getitem__(self, case_num, size=(128, 128, 128)):
         if self.is_validation == True:
             case_num += 200
-        img = self.df.loc[self.df['case_id'] == case_num]['image']
-        print("THIS IS WHAT WE ON")
-        print(img)
-        img = nib.load(img.values[0])
-        affine = img.affine
-        img = img.get_fdata()
-        img = resize_image(img, size)
-        img = np.expand_dims(img, axis=3)
-        img = np.expand_dims(img, axis=0)
-        #numpy2nifti(img, affine, 'img.nii.gz')
-        mask = self.df.loc[self.df['case_id'] == case_num]['mask']
-        mask = nib.load(mask.values[0])
-        mask = mask.get_data()
-        mask = resize_image(mask,size)
-        #numpy2nifti(mask, affine, 'mask.nii.gz')
-        mask = mask / 2
-        mask = np.expand_dims(mask, axis=3)
-        mask = np.expand_dims(mask, axis=0)
-        return img, mask
+        try:
+            print(self.path + '/' + "{}_i.npy".format(case_num))
+            img = np.load(self.path + '/' + "{}_i.npy".format(case_num))
+            mask = np.load(self.path + '/' +"{}_m.npy".format(case_num))
+            return img, mask
+        
+        except IOError, FileNotFoundError:
+            img = self.df.loc[self.df['case_id'] == case_num]['image']
+            #print("THIS IS WHAT WE ON")
+            img = nib.load(img.values[0])
+            affine = img.affine
+            img = img.get_fdata()
+            img = resize_image(img, size)
+            img = np.expand_dims(img, axis=3)
+            img = np.expand_dims(img, axis=0)
+            #numpy2nifti(img, affine, 'img.nii.gz')
+            mask = self.df.loc[self.df['case_id'] == case_num]['mask']
+            mask = nib.load(mask.values[0])
+            mask = mask.get_data()
+            mask = resize_image(mask,size)
+            #numpy2nifti(mask, affine, 'mask.nii.gz')
+            mask = mask / 2
+            mask = np.expand_dims(mask, axis=3)
+            mask = np.expand_dims(mask, axis=0)
+
+            np.save(self.path + '/' + "{}_i".format(case_num), img)
+            np.save(self.path + '/' + "{}_m".format(case_num), mask)
+            return img, mask
 
     def on_epoch_end(self):
         pass
 
     def __len__(self):
-       return 200
+       return self.df.shape[0]
 
-def load_data():
-    train_gen = DataGen("/home/kits/kits19/data/training")
+def load_data(path):
+    train_gen = DataGen(path)
     return train_gen
 
 def numpy2nifti(np_img, affine, name):
@@ -65,9 +74,7 @@ if __name__ == "__main__":
     data = DataGen("/home/kits/kits19/data/training")
     validation = DataGen("/home/kits/kits19/data/validation", is_validation=True)
     for i in range(0, 10):
-        img1, mask = validation.__getitem__(i)
+        img1, mask = data.__getitem__(i)
         print(img1.shape)
     
 
-    #print(img.get_data_dtype() == np.dtype(np.int8))
-    #print("Done")
--- train.py
+++ train.py
@@ -6,8 +6,11 @@ import numpy as np
 import cv2
 
 if __name__ == "__main__":
-    data_loader = load_data()
+    epochs = 1
+    data_loader = load_data("/home/kits/kits19/data/training")
+    validation_loader = load_data("/home/kits/kits19/data/validation", is_validation=True)
     model = load_model((128, 128, 128, 1))
     run_opts = tf.RunOptions(report_tensor_allocations_upon_oom = True)
     model.compile(optimizer='adam', loss="binary_crossentropy", metrics=["accuracy"])
-    model.fit_generator(data_loader, steps_per_epoch=10)
+    model.fit_generator(data_loader, validation_data=validation_loader, steps_per_epoch=200, validation_steps=10, epochs=epochs)
+    model.save_weights("UNetW.h5")
